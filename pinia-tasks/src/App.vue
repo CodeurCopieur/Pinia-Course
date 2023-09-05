97caa2f05962215f4cde6684f29dfed3bd39bd8f
Widdy LOUIS <louisws@outlook.fr>
2023-09-05 19:47:31 +0200

    first commit

---
+++ pinia-tasks/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <header class="bg-gray-300 pt-5 text-center flex justify-center items-center">
+      <img class="origin-top-left rotate-12" style="max-width: 60px;" src="./assets/pinia-logo.svg" alt="">
+      <h1 class="m-0 pt-6 ml-3.5 text-gray-500" style="rotate: 2deg;">Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<style>
+  body {
+    margin: 0;
+    font-family: 'Poppins';
+  }
+</style>
