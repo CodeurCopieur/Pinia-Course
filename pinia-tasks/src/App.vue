875ec229a7d7efe57be21fb195223427fe93cb86
Widdy LOUIS <louisws@outlook.fr>
2023-09-06 21:20:21 +0200

    Accessing Store State

--- pinia-tasks/src/App.vue
+++ pinia-tasks/src/App.vue
@@ -1,13 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+  /* imports */
+  import { useTaskStore } from './stores/taskStore'
+  import TaskDetails from './components/TaskDetails.vue'
+
+  const taskStore  = useTaskStore();
 </script>
 
 <template>
   <main>
+
+    <!-- heading -->
     <header class="bg-gray-300 pt-5 text-center flex justify-center items-center">
       <img class="origin-top-left rotate-12" style="max-width: 60px;" src="./assets/pinia-logo.svg" alt="">
       <h1 class="m-0 pt-6 ml-3.5 text-gray-500" style="rotate: 2deg;">Pinia Tasks</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list container max-w-3xl mx-auto text-white px-4">
+      <div 
+        v-for="(task, i) in taskStore.tasks" 
+        :key="task.id"
+        :class="{'mb-4': i !== taskStore.tasks.length - 1}">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
   </main>
 </template>
 
---
+++ pinia-tasks/src/components/TaskDetails.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+  /* imports */
+  import { Icon } from '@iconify/vue'
+
+  defineProps({
+    task: {
+      type: Object,
+      default: ()=> {}
+    },
+  });
+</script>
+
+<template>
+  <div 
+    class="flex items-center py-4 px-6 bg-weather-secondary rounded-md shadow-md cursor-pointer text-gray-500">
+    <div class="flex flex-col flex-1">
+      <h2 class="text-3xl my-3"> {{ task.title }}</h2>
+    </div>
+    <div class="flex flex-col gap-2">
+      <div class="flex gap-2">
+        <span class="text-xs">
+          <Icon icon="icon-park-outline:like" class="text-3xl" />
+        </span>
+        <span class="text-xs">
+          <Icon icon="lucide:trash-2" class="text-3xl" />
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
