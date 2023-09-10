4e80925208b1a4e2a9ceefa5d60c8ba037041aff
Widdy LOUIS <louisws@outlook.fr>
2023-09-10 13:14:09 +0200

    Actions

--- pinia-tasks/src/components/TaskDetails.vue
+++ pinia-tasks/src/components/TaskDetails.vue
@@ -2,6 +2,9 @@
 
   /* imports */
   import { Icon } from '@iconify/vue'
+  import { useTaskStore } from '../stores/taskStore'
+
+  const taskStore  = useTaskStore();
 
   defineProps({
     task: {
@@ -13,16 +16,22 @@
 
 <template>
   <div 
-    class="flex items-center py-4 px-6 bg-weather-secondary rounded-md shadow-md cursor-pointer text-gray-500">
+    class="flex items-center py-4 px-6 bg-weather-secondary rounded-md shadow-md text-gray-500">
     <div class="flex flex-col flex-1">
       <h2 class="text-3xl my-3"> {{ task.title }}</h2>
     </div>
     <div class="flex flex-col gap-2">
       <div class="flex gap-2">
-        <span class="text-xs">
-          <Icon icon="icon-park-outline:like" class="text-3xl" />
+        <span 
+          @click="taskStore.toggleTask(task.id)"
+          class="text-xs cursor-pointer">
+          <Icon icon="icon-park-outline:like" 
+            class="text-3xl"
+            :class="{'text-rose-500' : task.isFav}" />
         </span>
-        <span class="text-xs">
+        <span 
+          @click="taskStore.deleteTask(task.id)"
+          class="text-xs cursor-pointer">
           <Icon icon="lucide:trash-2" class="text-3xl" />
         </span>
       </div>
--- pinia-tasks/src/stores/taskStore.js
+++ pinia-tasks/src/stores/taskStore.js
@@ -18,6 +18,20 @@ export const useTaskStore = defineStore('taskStore', {
       totalCount: (state) =>{
         return state.tasks.length
       }
+    },
+    actions: {
+      addTask(task) {
+        this.tasks.push(task)
+      },
+      deleteTask(id) {
+        this.tasks = this.tasks.filter( t =>  {
+          return t.id != id
+        })
+      },
+      toggleTask(id) {
+        const task = this.tasks.find( t => t.id === id)
+        task.isFav = !task.isFav
+      }
     }
   
 })
